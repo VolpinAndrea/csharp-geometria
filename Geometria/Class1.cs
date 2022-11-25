7ec292f8a6fd6a50bdc69fd4f14f74eea9c03d33
VolpinAndrea <andvolpin19@gmail.com>
2022-11-25 14:35:24 +0100

    Oggetto e punti 1. e 2.

---
+++ Geometria/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    internal class Rettangolo
+    {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public int calcolaArea(int baseR, int altezzaR) 
+        {
+            return baseR * altezzaR;
+        }
+        public int calcolaPerimetro(int baseR, int altezzaR)
+        {
+            return 2*(baseR + altezzaR);
+        }
+    }
+}
--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Geometria;
+
+Rettangolo rUno = new Rettangolo();
+Console.WriteLine("Inserisci l'altezza del rettangolo");
+rUno.altezzaRettangolo = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci la base del rettangolo");
+rUno.baseRettangolo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("PERIMETRO:");
+Console.WriteLine(rUno.calcolaPerimetro(rUno.baseRettangolo, rUno.altezzaRettangolo));
+Console.WriteLine("AREA:");
+Console.WriteLine(rUno.calcolaArea(rUno.baseRettangolo, rUno.altezzaRettangolo));
